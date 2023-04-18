0218059aedf51869fc78466ced7a34f8472fd0e5
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-18 11:19:58 +0300

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,8 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+
+const express = require('express')
+
+const Posts = require('./posts/posts-model')
+const postsRouter = require('./posts/posts-router')
